a8e8c96abd58328125a0c21a3d5bdde437b7de9e
JedrzejZygadlo <jedrekzygadlo@wp.pl>
2019-09-19 03:19:03 +0200

    Style Single Album Page
    Add TrackList component to reuse in future

---
+++ src/components/TrackList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledTrackListGrid = styled.div `
+    display: grid;
+    grid-template-columns: 100px 400px 100px;
+    border-bottom: 1px solid grey;
+    justify-content: center;
+`;
+const StyledText = styled.p `
+    color: white;
+`;
+const countToMinSek = (ms) => {
+        var sek = ms/1000;
+        const min = Math.floor(sek / 60);
+        sek= parseInt(sek % 60);
+        if(sek<10){
+            sek = '0' + sek;
+        }
+        const result = min + ':' + sek;
+        return result 
+    }
+const renderTracks = (tracks) => {
+    return tracks.map(track => {
+        return(
+            <StyledTrackListGrid>
+                    <StyledText>{track.track_number}</StyledText>
+                    <StyledText>{track.name}</StyledText>
+                    <StyledText>{countToMinSek(track.duration_ms)}</StyledText>
+            </StyledTrackListGrid>
+        )
+    })
+}
+const TrackList = (props) => {
+    console.log(props);
+    return(
+        <>
+            <StyledTrackListGrid>
+                    <StyledText>Numer</StyledText>
+                    <StyledText>Tytuł</StyledText>   
+                    <StyledText>Długość</StyledText>
+            </StyledTrackListGrid>
+            {renderTracks(props.tracks)}
+        </>
+    )
+}
+
+export default TrackList;
